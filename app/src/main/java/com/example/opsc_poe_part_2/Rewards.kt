d008d993ba6789e7322d63c81b2cd9d9cd3a7c47
CP Kruger <christoffelpetruskr@gmail.com>
2024-09-26 18:25:32 +0200

    Adding Rewards and Level up

--- app/src/main/java/com/example/opsc_poe_part_2/Rewards.kt
+++ app/src/main/java/com/example/opsc_poe_part_2/Rewards.kt
@@ -1,6 +1,8 @@
 package com.example.opsc_poe_part_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,21 @@ class Rewards : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnDailyGoals = findViewById<Button>(R.id.btnDailyGoals)
+        val btnLevelUp = findViewById<Button>(R.id.btnLevelUp)
+
+        btnDailyGoals.setOnClickListener {
+            // Create an intent to start RegisterActivity
+            val intent = Intent(this, DailyGoals::class.java)
+            startActivity(intent)
+        }
+
+        btnLevelUp.setOnClickListener {
+            // Create an intent to start RegisterActivity
+            val intent = Intent(this, LevelUp::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
