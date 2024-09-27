3049f39860fd6a26549496345350634d9ad3c808
garthdp <garthdp4@gmail.com>
2024-09-27 14:19:05 +0200

    fixed

--- app/src/main/java/com/example/opsc_poe_part_2/AddDiaryEntry.kt
+++ app/src/main/java/com/example/opsc_poe_part_2/AddDiaryEntry.kt
@@ -6,6 +6,24 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.Date
+import java.util.concurrent.Executors
 
 class AddDiaryEntry : AppCompatActivity(), EmojiPickerDialogFragment.EmojiPickerListener {
     private lateinit var saveButton: Button
@@ -33,13 +51,20 @@ class AddDiaryEntry : AppCompatActivity(), EmojiPickerDialogFragment.EmojiPicker
             val content = contentEditText.text.toString()
 
             // Create a new DiaryEntry object
-            val newEntry = DiaryEntry(title, content, selectedEmojiResId)
+            //val newEntry = DiaryEntry(title, content, selectedEmojiResId)
 
             // Save to a static list
-            DiaryEntries.entries.add(newEntry)
+            //DiaryEntries.entries.add(newEntry)
 
             // Set result to OK and pass back the new entry (optional)
             setResult(RESULT_OK)
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val check = async { patchRequest() }
+                check.await()
+                val addUser = async { postRequest(content, title, selectedEmojiResId) }
+                addUser.await()
+            }
             finish()
         }
 
@@ -49,6 +74,67 @@ class AddDiaryEntry : AppCompatActivity(), EmojiPickerDialogFragment.EmojiPicker
         selectedEmojiResId = emojiResId
         emojiTextView.setCompoundDrawablesWithIntrinsicBounds(0, emojiResId, 0, 0)
     }
+
+    suspend fun patchRequest() {
+        /*
+            Code Attribution
+            Title: How to use the OkHttp library in Android Studio
+            Author: Codes Easy
+            Link: https://www.youtube.com/watch?v=uSY2RqdBL04
+            Usage: learned how to make patch api requests
+        */
+        val client = OkHttpClient()
+
+        val url = "https://opscmeditationapi.azurewebsites.net/api/users/updateLevel".toHttpUrlOrNull()!!.newBuilder()
+            .addQueryParameter("email", userEmail)
+            .addQueryParameter("experience", "50")
+            .build()
+
+        // Create a dummy request body as PATCH requires a body, even if it's empty
+        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), "")
+
+        val request = Request.Builder().url(url).patch(requestBody).build()
+        val executor = Executors.newSingleThreadExecutor()
+
+        executor.execute{
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                println(response.body?.string())
+            }
+        }
+        delay(3000)
+    }
+
+    suspend fun postRequest(content: String, title: String, emoji: Int) {
+        val client = OkHttpClient()
+
+        val sdf = SimpleDateFormat("dd/MM/yyyy")
+        val currentDate = sdf.format(Date())
+        val url = "https://opscmeditationapi.azurewebsites.net/api/Journal".toHttpUrlOrNull()!!.newBuilder()
+            .addQueryParameter("email", userEmail)
+            .addQueryParameter("date", currentDate)
+            .addQueryParameter("content", content)
+            .addQueryParameter("emoji", emoji.toString())
+            .addQueryParameter("title", title)
+            .build()
+
+
+        // Create a dummy request body as PATCH requires a body, even if it's empty
+        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), "")
+
+        val request = Request.Builder().url(url).post(requestBody).build()
+        val executor = Executors.newSingleThreadExecutor()
+
+        executor.execute{
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                println(response.body?.string())
+            }
+        }
+        delay(3000)
+    }
 }
 
 // Singleton object to hold diary entries
@@ -56,7 +142,9 @@ object DiaryEntries {
     val entries = mutableListOf<DiaryEntry>()
 }
 data class DiaryEntry(
-    val title: String,
-    val content: String,
-    val emojiResId: Int
+    val emoji: Int,
+    val EntryId: String,
+    val Content: String,
+    val Title: String,
+    val Date: String
 )
--- app/src/main/java/com/example/opsc_poe_part_2/Dairy.kt
+++ app/src/main/java/com/example/opsc_poe_part_2/Dairy.kt
@@ -2,30 +2,29 @@ package com.example.opsc_poe_part_2
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import java.net.URL
+import java.util.concurrent.Executors
+import kotlin.math.log
 
 class Dairy : AppCompatActivity() {
     private lateinit var addEntryButton: Button
-    private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: DiaryEntryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dairy)
-
         addEntryButton = findViewById(R.id.button5)
-        recyclerView = findViewById(R.id.recycler_view_entries)
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Initialize and set the adapter
-        adapter = DiaryEntryAdapter(DiaryEntries.entries)
-        recyclerView.adapter = adapter
-
-        // Call to display existing diary entries
-        displayDiaryEntries()
+        getDiaryEntries()
 
         addEntryButton.setOnClickListener {
             val intent = Intent(this, AddDiaryEntry::class.java)
@@ -37,12 +36,46 @@ class Dairy : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == ADD_ENTRY_REQUEST && resultCode == RESULT_OK) {
             // Notify the adapter about the new entry and refresh the entries displayed
-            displayDiaryEntries()
+            getDiaryEntries()
         }
     }
 
     private fun displayDiaryEntries() {
         adapter.notifyDataSetChanged() // Notify the adapter to refresh the views
+    }
+    override fun onResume() {
+        super.onResume()
+        // Fetch and display the diary entries every time the page is reloaded (resumed)
+        getDiaryEntries()
+    }
+
+    private fun getDiaryEntries(){
+        val executor = Executors.newSingleThreadExecutor()
+        val recyclerView : RecyclerView = findViewById(R.id.recycler_view_entries)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        executor.execute {
+            try {
+                val url = URL("https://opscmeditationapi.azurewebsites.net/api/Journal/GetJournalEntries?email=${userEmail}")
+                val json = url.readText()
+                if(json.equals("null")){
+                    Log.d("Error", "Nothing found")
+                }
+                else{
+                    val dairyResponse = Gson().fromJson(json, Array<DiaryEntry>::class.java)
+                    Log.d("Response", dairyResponse[0].toString())
+                    Handler(Looper.getMainLooper()).post{
+                        // Initialize and set the adapter
+                        adapter = DiaryEntryAdapter(dairyResponse)
+                        recyclerView.adapter = adapter
+
+                        // Call to display existing diary entries
+                        displayDiaryEntries()
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("Error", "Fetch error occured")
+            }
+        }
     }
 
     companion object {
