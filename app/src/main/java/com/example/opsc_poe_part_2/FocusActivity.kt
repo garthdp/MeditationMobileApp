0c68981d8ac6960c4d951aba8822f87c96934bb4
Colby Botes <ST10145691@vcconnect.edu.za>
2024-09-28 00:07:10 +0200

    changed back button to match layout, added video feauture to all the meditation pages

--- app/src/main/java/com/example/opsc_poe_part_2/FocusActivity.kt
+++ app/src/main/java/com/example/opsc_poe_part_2/FocusActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 
 class FocusActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_focus) // Ensure you have the layout file for music page
+        setContentView(R.layout.activity_focus)
 
+        val videoList = listOf(
+            R.raw.focus
+        )
 
-        // Find the back button in the layout
         val backButton=findViewById<ImageButton>(R.id.backButton)
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val adapter = VideoAdapter(this, videoList)
+        viewPager.adapter = adapter
 
         // Set an OnClickListener on the back button
         backButton.setOnClickListener {
@@ -20,7 +27,7 @@ class FocusActivity: AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             // Start MainActivity
             startActivity(intent)
-            // Optionally finish MusicActivity to remove it from the back stack
+
             finish()
         }
     }
